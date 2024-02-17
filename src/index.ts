ca4f8420085dfca6db4009a7d2b57a5a724fa923
H4M5TER <zhuxx2003@gmail.com>
2024-02-17 14:51:04 +0800

    v1.0.0

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'encodeuri'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const factory = (f: (string) => string) => {
+    ctx.command(`${f.name.slice(0, 6)}.${f.name.slice(6)} <text:text>`)
+      .action((argv, text) => {
+        return f(text)
+      })
+  }
+  [encodeURI, decodeURI, encodeURIComponent, decodeURIComponent].forEach(f => factory(f))
 }
